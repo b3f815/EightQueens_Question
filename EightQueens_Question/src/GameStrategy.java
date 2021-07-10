64230adead9fba49ef234c22837af6ab2b4f9f40
Befin <Befin@DESKTOP-3PN5QHO>
2021-07-10 00:01:29 -0700

    complted Fuctions

--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -4,14 +4,14 @@ public class GameStrategy {
 
 	private int getColumn(int cellId) {
 		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+		
+		return cellId%8;	// just for the heck of it
 	}
 	
 	private int getRow(int cellId) {
 		// WRITE YOUR LOGIC HERE....................................
 		
-		return 0;	// just for the heck of it
+		return cellId/8	;	// just for the heck of it
 	}
 
 	public boolean isValidPosition(int cellId) {
@@ -25,10 +25,36 @@ public class GameStrategy {
 		
 		int row = getRow(cellId);
 		
-		/*
-			WRITE YOUR LOGIC HERE...............................
-
-		*/
+		int i,j;
+		for (i = 0;i < 8;i++) {//to check if the row is free
+			if(placedQueens[i][col] == true) {
+				isValid = false;
+			}}
+		for(j = 0;j<8;j++) {
+			if(placedQueens[row][j] == true) {//to check column
+				isValid = false;
+			}}
+		for (i = row,j=col;i>=0 && j>=0;i--,j--) {//to check tower right diagonal
+			if(placedQueens[i][j] == true) {
+				isValid = false;
+			}}
+		for (i = row,j=col;i<8 && j<8;i++,j++) {//to check upper right diagonal
+			if(placedQueens[i][j] == true) {
+				isValid = false;
+			}}
+		for (i = row,j=col;j>=0 && i<8;i++,j--) {//to check upper right diagonal
+			if(placedQueens[i][j] == true) {
+				isValid = false;
+			}}
+		for (i = row,j=col;i>=0 && j<8;i--,j++) {//to check lower right diagonal 
+			if(placedQueens[i][j] == true) {
+				isValid = false;
+			}}
+		
+		if(isValid) {
+			placedQueens[row][col] = true;
+			numQueens++;
+		}
 		return isValid;
 	}
 }
